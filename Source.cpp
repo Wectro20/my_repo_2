5905c08ddcafe560019235b65feca2ee5743bfe9
Wectro20 <74012654+Wectro20@users.noreply.github.com>
2020-12-13 20:01:55 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,16 +3,16 @@
 
 int main() {
     Dinner day(1, 12.0, 7);
-    day.initcandysinarray(Candy("BAR", 24, 4, 100, BAR), 0);
-    day.initcandysinarray(Candy("BUTTON", 0.1, 4, 26.5, BUTTON), 1);
-    day.initcandysinarray(Candy("POPCORN", 0.3, 4, 75.5, POPCORN), 2);
-    day.initcandysinarray(Candy("GUM", 0.4, 4, 95, GUM), 3);
-    day.initcandysinarray(Candy("SNICKERS", 0.2, 4, 30, SNICKERS), 4);
-    day.initcandysinarray(Candy("BOUNTY", 0.3, 4, 48, BOUNTY), 5);
-    day.initcandysinarray(Candy("MILKYWAY", 0.1, 4, 12, MILKYWAY), 6);
-    day.getfromarraycandy(0).ate();
-    day.findthemostexpensivecandies("Rob joi");
-    day.getfromarraycandy(0).print();
+    day.initCandysInArray(Candy("BAR", 24, 4, 100, BAR), 0);
+    day.initCandysInArray(Candy("BUTTON", 0.1, 4, 26.5, BUTTON), 1);
+    day.initCandysInArray(Candy("POPCORN", 0.3, 4, 75.5, POPCORN), 2);
+    day.initCandysInArray(Candy("GUM", 0.4, 4, 95, GUM), 3);
+    day.initCandysInArray(Candy("SNICKERS", 0.2, 4, 30, SNICKERS), 4);
+    day.initCandysInArray(Candy("BOUNTY", 0.3, 4, 48, BOUNTY), 5);
+    day.initCandysInArray(Candy("MILKYWAY", 0.1, 4, 12, MILKYWAY), 6);
+    day.getFromArrayCandy(0).ate();
+    day.findTheMostExpensiveCandies("Rob joi");
+    day.getFromArrayCandy(0).print();
 
     return 0;
 }
